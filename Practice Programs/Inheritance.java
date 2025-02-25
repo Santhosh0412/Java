b2266c2d10a6a97de1c0dea7d1f404c450795cfc
Santhosh0412 <santhosh2005122@gmail.com>
2025-02-25 21:41:29 +0530

    Created a new file

---
+++ Practice Programs/Inheritance.java
@@ -0,0 +1,46 @@
+class Vehicle {
+    String Brand;
+    int Year;
+    void startEngine() {
+        System.out.println("Vehicle engine starts");
+    }
+}
+
+class Car extends Vehicle {
+    String FuelType;
+    void startEngine() {
+        System.out.println("Car engine starts");
+    }
+    void drive() {
+        System.out.println("Car is driving");
+    }
+}
+class Truck extends Vehicle {
+    int loadCapacity;
+    void startEngine() {
+        System.out.println("Truck engine starts");
+    }
+    void haul() {
+        System.out.println("Truck is hauling");
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Car obj = new Car();
+        obj.Brand = "Hyundai";
+        System.out.println(obj.Brand);
+        obj.FuelType = "Petrol";
+        obj.Year = 2022;
+
+        obj.startEngine();
+        obj.drive();
+
+        Truck truckObj = new Truck();
+        truckObj.Brand = "Volvo";
+        truckObj.loadCapacity = 10000;
+        truckObj.Year = 2021;
+
+        truckObj.startEngine();
+        truckObj.haul();
+    }
+}
