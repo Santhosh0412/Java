88c477be71757658df3c7f72522a1118ad8006cb
Santhosh0412 <santhosh2005122@gmail.com>
2025-03-18 12:31:56 +0530

    Created a new file

--- Practice Programs/Frequency.java
+++ Practice Programs/Frequency.java
@@ -32,14 +32,15 @@ public class Frequency {
         }
     }
     static void convertBinary(){
-        int binary[]=new int[40];
+        int binary[]=new int[32];
+        int n=binary.length;
         int index=0;
         int num=12;
         while(num>0){
             binary[index++]=num%2;
             num=num/2;
         }
-        for(int i=index-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             System.out.print(binary[i]);
         }
     }
