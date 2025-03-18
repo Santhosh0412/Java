6956765407d3238b456d4ea6d157b8a1a9bb5f3e
Santhosh0412 <santhosh2005122@gmail.com>
2025-03-18 11:53:55 +0530

    Created a new file

--- Practice Programs/Frequency.java
+++ Practice Programs/Frequency.java
@@ -1,21 +1,39 @@
+import java.util.HashMap;
+
 public class Frequency {
-    public static void main(String[]args){
-        int arr[]={6,8,2,1};
+    static void sort(){
+        int arr[] ={10,5,4,2};
         int n=arr.length;
         int temp;
-       for(int i=0;i<=n-i;i++){
-        for(int j=i;j<=n-1;j++){
-            if(arr[i]>arr[j]){
-            temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+        for(int i=0;i<=n;i++){
+            for(int j=i;j<=n-1;j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+                
+            }
         }
-        
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
     }
+    static void freq(){
+        String fre = "Thunderthun";
+        char arr[] = fre.toCharArray();
+        int n = arr.length;
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char c : arr) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+        for (char key : freqMap.keySet()) {
+            System.out.println(key + " -> " + freqMap.get(key));
+        }
     }
-    for(int i:arr){
-        System.out.print(i+" ");
-    }    
+    public static void main(String args[]){
+       sort();
+       System.out.println(); 
+       freq();
     }
-    
 }
