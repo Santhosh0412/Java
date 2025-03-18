fd572fc4e4ce815b9b8113211cedfcf3c0294385
Santhosh0412 <santhosh2005122@gmail.com>
2025-03-18 18:18:10 +0530

    Created a new file

---
+++ Practice Programs/Ispalindrome.java
@@ -0,0 +1,23 @@
+public class  Ispalindrome {
+    public boolean isPalindrome(int x) {
+       if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int x = 121;
+        System.out.println(solution.isPalindrome(x));  
+        x = -121;
+        System.out.println(solution.isPalindrome(x));  
+         x = 10;
+        System.out.println(solution.isPalindrome(x));  
+    }
+}
