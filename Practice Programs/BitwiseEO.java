75433cfcf1f68137575dc5a9ccb66bf55c735180
Santhosh0412 <santhosh2005122@gmail.com>
2025-03-27 10:37:36 +0530

    Created a new file

---
+++ Practice Programs/BitwiseEO.java
@@ -0,0 +1,12 @@
+public class BitwiseEO{
+    public static void main(String[]args){
+        int num=5;
+        int result=num | 1 ;
+        if(result>num){
+            System.out.print("Even");
+        }else{
+            System.out.print("Odd");
+        }
+    }
+    
+}
